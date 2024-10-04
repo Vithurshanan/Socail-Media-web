94112fe8b6b4d1fd631a3c2f9dcbc693d6335a67
Ananthakumar Vithurshanan <lithuvithu1109@gmail.com>
2024-10-05 00:04:50 +0530

    Adding Navbar and Login using clerk

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="h-[calc(100vh-96px)] flex items-center justify-center">
+      <SignUp />
+    </div>
+  );
+}
