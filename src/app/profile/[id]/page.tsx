b1620b58fe28eabb324b48a9efb731c870ceaa18
Ananthakumar Vithurshanan <lithuvithu1109@gmail.com>
2024-10-06 12:00:10 +0530

    Adding left menu sections

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,16 +1,51 @@
 import Feed from '@/components/Feed'
 import LeftMenu from '@/components/LeftMenu'
 import RightMenu from '@/components/RightMenu'
+import Image from 'next/image'
 import React from 'react'
 
 const Profile = () => {
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
-        <LeftMenu  />
+        <LeftMenu  type='profile'/>
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src='https://images.pexels.com/photos/28663937/pexels-photo-28663937/free-photo-of-urban-street-photography-in-mumbai-india.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+                alt=""
+                fill
+                className="rounded-md object-cover"
+              />
+              <Image
+                src='https://images.pexels.com/photos/28107024/pexels-photo-28107024/free-photo-of-a-child-holding-hands-with-an-adult-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">
+              
+            </h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">30</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">5.5K</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">3K</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
           <Feed />
         </div>
       </div>
